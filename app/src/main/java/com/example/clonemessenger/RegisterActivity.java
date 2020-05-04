12504e18539fcf12c0d884adbedc0fad17e4d783
Han <philong07712@gmail.com>
2020-05-04 22:30:14 +0700

    init firebase

---
+++ app/src/main/java/com/example/clonemessenger/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.clonemessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
