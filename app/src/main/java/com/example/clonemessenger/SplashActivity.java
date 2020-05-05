0365cf4500d50ec9b81e2a97345ffd7115e173e0
Han <philong07712@gmail.com>
2020-05-05 10:29:18 +0700

    Create login activity

--- app/src/main/java/com/example/clonemessenger/SplashActivity.java
+++ app/src/main/java/com/example/clonemessenger/SplashActivity.java
@@ -2,12 +2,19 @@ package com.example.clonemessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Button login;
+    TextView register;
 
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -16,6 +23,27 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Init view
+        login = findViewById(R.id.splash_login);
+        register = findViewById(R.id.splash_register);
 
+        setListener();
     }
+
+    private void setListener()
+    {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateLogin();
+            }
+        });
+    }
+
+    private void updateLogin()
+    {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
